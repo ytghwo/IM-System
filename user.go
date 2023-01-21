7977421bfda6febd7784ad47b66f5e963cfccdcf
pxw <08213064@cumt.edu.cn>
2023-01-21 16:42:41 +0800

    新增在线用户查询和修改用户名功能

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name   string
@@ -45,9 +48,35 @@ func (user *User) Offline() {
 	user.server.mapLock.Lock()
 	delete(user.server.OnlineMap, user.Name)
 	user.server.mapLock.Unlock()
-	user.server.BroadCast(user, "is downline")
+	user.server.BroadCast(user, "is downline\n")
 }
 
 func (user *User) DoMessage(msg string) {
-	user.server.BroadCast(user, msg)
+	if msg == "who" {
+		user.server.mapLock.Lock()
+		for _, users := range user.server.OnlineMap {
+			onlineMsg := "[" + users.Addr + "]" + users.Name + ": is online...\n"
+			user.sendMsg(onlineMsg)
+		}
+		user.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		_, ok := user.server.OnlineMap[newName]
+		if ok {
+			user.sendMsg("this name is used\n")
+		} else {
+			user.server.mapLock.Lock()
+			delete(user.server.OnlineMap, user.Name)
+			user.server.OnlineMap[newName] = user
+			user.server.mapLock.Unlock()
+			user.Name = newName
+			user.sendMsg("rename is ok\n")
+		}
+	} else {
+		user.server.BroadCast(user, msg)
+	}
+}
+
+func (user *User) sendMsg(msg string) {
+	user.conn.Write([]byte(msg))
 }
