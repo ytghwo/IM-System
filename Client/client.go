b8bf9d2d5908c2e0b6f18a0ba6bc77703956bbfb
pxw <08213064@cumt.edu.cn>
2023-01-27 16:36:32 +0800

    完成客户端相关操作

--- Client/client.go
+++ Client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -10,12 +13,14 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
 	if err != nil {
@@ -26,12 +31,127 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "set ip(default ip is 127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "set port(default port is 8888)")
+}
+
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.public chat")
+	fmt.Println("2.private chat")
+	fmt.Println("3.update the user name")
+	fmt.Println("0.exit")
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("please enter a valid number")
+		return false
+	}
+}
+
+func (client *Client) UpdateName() {
+	fmt.Println(">>>>>please enter username")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+}
+
+func (client *Client) PublicChat() {
+	var chatMsg string
+	fmt.Println(">>>>>please enter messege")
+	fmt.Scanln(&chatMsg)
+	for chatMsg != "exit" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn Write err", err)
+				break
+			}
+		}
+		chatMsg = ""
+		fmt.Println(">>>>>please enter messege")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+	client.SelectUsers()
+	fmt.Println(">>>>>>>please enter name")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>>>>please enter messege")
+		fmt.Scanln(&chatMsg)
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+			}
+			chatMsg = ""
+			fmt.Println(">>>>>>please enter messege")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>>>please enter name")
+		fmt.Scanln(&remoteName)
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for !client.menu() {
+
+		}
+		switch client.flag {
+		case 1:
+			client.PublicChat()
+		case 2:
+			client.PrivateChat()
+		case 3:
+			client.UpdateName()
+		}
+	}
+
+}
+
+func (client *Client) DealResponse() {
+	io.Copy(os.Stdout, client.conn)
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>>>> 连接服务器失败...")
 		return
 	}
+	go client.DealResponse()
 	fmt.Println(">>>>>>>> 连接服务器成功...")
-	select {}
+	client.Run()
 }
